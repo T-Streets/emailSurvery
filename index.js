5c2576509b508e1b32c9cb1ea09ca57b2eaffdf6
Tyler Ceja <tylerceja@tstreets.local>
2018-04-17 01:22:25 -0700

    defined OAuth strategies

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express()
+const keys = require('./config/keys')
 
-app.get('/', (req,res) => {
-    res.send('see you space cowboy')
-})
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback" // where the user is sent after auth
+}, (accessToken) => {
+    console.log(accessToken)
+}))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
