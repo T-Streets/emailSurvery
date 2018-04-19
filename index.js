b5922f0919ed07f48c9e225912d41e3368ec3d32
T-Streets <tyler.ceja5@gmail.com>
2018-04-19 15:36:11 -0700

    connects to mongoDB with mongoose/creates user schema

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+const app = express()
+require('./models/user')
 require('./services/passport')
 
-const app = express()
+mongoose.connect(keys.mongoURI)
 
 require('./routes/authRoutes')(app)
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
