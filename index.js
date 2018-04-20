172597b1ea3c1e2fe968b8bb75f5181681a7b0ef
T-Streets <tyler.ceja5@gmail.com>
2018-04-19 23:36:10 -0700

    sets cookie and creates logic to save user to DB

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 const keys = require('./config/keys')
 const app = express()
 require('./models/user')
@@ -9,6 +11,15 @@ mongoose.connect(keys.mongoURI)
 
 require('./routes/authRoutes')(app)
 
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 + 1000, //30days
+        keys: [keys.cookieKey], //encrypts cookie
+    })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 const PORT = process.env.PORT || 5000
