b56a09a6db3f34aaeb7517349a15512633e213df
T-Streets <tyler.ceja5@gmail.com>
2018-04-24 14:21:19 -0700

    adds reducers/store

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
 import App from './Components/App'
+import reducers from './Reducers'
+
+const store = createStore(reducers, {}, applyMiddleware)
 
 ReactDOM.render(
-<App />,
+<Provider store={store}><App /></Provider>, 
 document.querySelector('#root')
 )
