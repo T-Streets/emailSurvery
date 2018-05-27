6203c2db8b9b0729c5a8846c277a76e5153dfa58
T-Streets <tyler.ceja5@gmail.com>
2018-05-27 00:09:48 -0700

    adds heroku build dependenices

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(keys.mongoURI)
 app.use(bodyParser.json());
 
 /**
- *Middleware sets and and encrypts cookie for 30 days
+ *middleware sets and and encrypts cookie for 30 days
  */
 app.use(
     cookieSession({
@@ -26,7 +26,7 @@ app.use(
 )
 
 /**
- *Middleware initiates passport to use cookies for OAuth
+ *middleware initiates passport to use cookies for OAuth
  */
 app.use(passport.initialize())
 app.use(passport.session())
@@ -35,6 +35,18 @@ app.use(passport.session())
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+/**
+ * when in production, express serves production assets
+ * if route is not recognized, express will serve index.html
+ */
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
